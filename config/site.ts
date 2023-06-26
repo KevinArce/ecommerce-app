034ba8b129ca065b37e667545e69e474cf4c3ed7
KevinArce <iav.kevinarce@ufg.edu.sv>
2023-06-25 20:18:52 -0600

    first commit

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Superheroes and Villains - Finder",
+  description: "Get all SuperHeroes and Villians data from all universes!",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
